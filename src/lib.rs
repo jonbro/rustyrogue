395f999b1ac6281acdf6b8469b7eb409385e924b
Jonathan Brodsky <jonbro@gmail.com>
2020-06-18 14:42:19 -0400

    switched over to wasm-pack

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use wasm_bindgen::prelude::*;
 use rltk::{Rltk, GameState};
 
 struct State {}
@@ -10,10 +11,17 @@ impl GameState for State {
 
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
-    
     let context = RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
         .build()?;
     let gs = State{ };
     rltk::main_loop(context, gs)
+}
+
+// entry point for the wasm-pack module
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue>
+{
+    let _e = main();
+    Ok(())
 }
