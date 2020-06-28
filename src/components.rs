5cfc4fd2c3142fc0e689c8741e76298e939e8cdb
Jonathan Brodsky <jonbro@gmail.com>
2020-06-28 11:48:04 -0700

    enemies chase player

--- src/components.rs
+++ src/components.rs
@@ -17,3 +17,29 @@ pub struct Renderable {
 
 #[derive(Component, Debug)]
 pub struct Player {}
+
+#[derive(Component)]
+pub struct Viewshed {
+    pub visible_tiles : Vec<rltk::Point>,
+    pub range : i32,
+    pub dirty : bool
+}
+
+#[derive(Component, Debug)]
+pub struct Monster {}
+
+#[derive(Component, Debug)]
+pub struct Name {
+    pub name : String
+}
+
+#[derive(Component, Debug)]
+pub struct BlocksTile {}
+
+#[derive(Component, Debug)]
+pub struct CombatStats {
+    pub max_hp : i32,
+    pub hp : i32,
+    pub defense : i32,
+    pub power : i32
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::prelude::*;
 
-use rltk::{GameState, Rltk, RGB};
+use rltk::{GameState, Rltk, RGB, Point};
 use specs::prelude::*;
 
 mod components;
@@ -11,13 +11,29 @@ mod player;
 use player::*;
 mod rect;
 pub use rect::Rect;
+mod monster_ai_system;
+use monster_ai_system::MonsterAI;
+mod visibility_system;
+use visibility_system::VisibilitySystem;
+mod map_indexing_system;
+use map_indexing_system::MapIndexingSystem;
+
+#[derive(PartialEq, Copy, Clone)]
+pub enum RunState { Paused, Running }
 
 pub struct State {
-    pub ecs: World
+    pub ecs: World,
+    pub runstate : RunState
 }
 
 impl State {
     fn run_systems(&mut self) {
+        let mut vis = VisibilitySystem{};
+        vis.run_now(&self.ecs);
+        let mut mob = MonsterAI{};
+        mob.run_now(&self.ecs);
+        let mut mapindex = MapIndexingSystem{};
+        mapindex.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -25,35 +41,46 @@ impl State {
 impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
         ctx.cls();
-        player_input(self, ctx);
+        if self.runstate == RunState::Running {
+            self.run_systems();
+            self.runstate = RunState::Paused;
+        } else {
+            self.runstate = player_input(self, ctx);
+        }
+        
+        draw_map(&self.ecs, ctx);
 
-        self.run_systems();
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
-        
-   
-        let map = self.ecs.fetch::<Vec<TileType>>();
-        draw_map(&map, ctx);
+        let map = self.ecs.fetch::<Map>();
         for (pos, render) in (&positions, &renderables).join() {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            let idx = map.xy_idx(pos.x, pos.y);
+            if map.visible_tiles[idx] {ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph) }
         }
     }
 }
 
 pub fn roguemain() -> rltk::BError {
     use rltk::RltkBuilder;
-    let context = RltkBuilder::simple80x50()
+    let mut context = RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
         .build()?;
+    context.with_post_scanlines(true);
     let mut gs = State{
-        ecs: World::new()
+        ecs: World::new(),
+        runstate: RunState::Running
     };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
-    let (rooms, map) = new_map_rooms_and_corridors();
-    gs.ecs.insert(map);
-    let (player_x, player_y) = rooms[0].center();
+    gs.ecs.register::<Viewshed>();
+    gs.ecs.register::<Monster>();
+    gs.ecs.register::<Name>();
+    gs.ecs.register::<BlocksTile>();
+    gs.ecs.register::<CombatStats>();
+
+    let map : Map = Map::new_map_rooms_and_corridors();
+    let (player_x, player_y) = map.rooms[0].center();
     gs.ecs
         .create_entity()
         .with(Position { x: player_x, y: player_y })
@@ -63,8 +90,36 @@ pub fn roguemain() -> rltk::BError {
             bg: RGB::named(rltk::BLACK),
         })
         .with(Player{})
+        .with(Viewshed{ visible_tiles : Vec::new(), range : 8, dirty : false })
+        .with(Name{name: "Player".to_string()})
+        .with(CombatStats{ max_hp: 30, hp: 30, defense: 2, power: 5})
         .build();
-
+    let mut rng = rltk::RandomNumberGenerator::new();
+    for (i, room) in map.rooms.iter().skip(1).enumerate() {
+        let (x,y) = room.center();
+        let glyph : rltk::FontCharType;
+        let name : String;
+        let roll = rng.roll_dice(1,2);
+        match roll {
+            1 => { glyph = rltk::to_cp437('g'); name = "Goblin".to_string(); }
+            _ => { glyph = rltk::to_cp437('o'); name = "Orc".to_string(); }
+        }
+        gs.ecs.create_entity()
+            .with(Position{x,y})
+            .with(Renderable{
+                glyph: glyph,
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK)
+            })
+            .with(Viewshed{ visible_tiles: Vec::new(), range: 8, dirty: true})
+            .with(Monster{})
+            .with(Name{ name: format!("{} #{}", &name, i)})
+            .with(BlocksTile{})
+            .with(CombatStats{ max_hp: 16, hp: 16, defense: 1, power: 4 })
+            .build();
+    }
+    gs.ecs.insert(map);
+    gs.ecs.insert(Point::new(player_x, player_y));
     rltk::main_loop(context, gs)
 }
 
